754c9eb7e8020beb747157a4df78363706dad523
Matheus <matheusvrgo2@gmail.com>
2025-02-01 13:07:42 -0300

    adicionar dotenv

--- src/assets/Home/Home.tsx
+++ src/assets/Home/Home.tsx
@@ -1,12 +1,17 @@
 import Navbar from '../../components/Navbar'
 import SwiperComponent from '../../components/Swiper.tsx';
 import './Home.css';
+const secret_key = import.meta.env.VITE_APIKEY
 
 export default function Home() {
+  console.log(secret_key)
   return (
     <>
+      
       <Navbar />
       <SwiperComponent />
+
+      <h1 className='title'>Mais populares</h1>
     </>
   )
 }
