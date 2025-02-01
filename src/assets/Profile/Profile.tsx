c318746d1e26306537b2726669916e4de880fb17
Matheus <matheusvrgo2@gmail.com>
2025-02-01 16:38:02 -0300

    adicionar profile page e iniciando footer

--- src/assets/Profile/Profile.tsx
+++ src/assets/Profile/Profile.tsx
@@ -5,7 +5,17 @@ export default function Profile() {
   return (
     <>
       <Navbar />
-      Teste
+      <div className="loginContainer">
+        <div className="loginContent">
+          <h1>Login</h1>
+          <form action="">
+            <input type="text" placeholder='Email' />
+            <input type="password" placeholder='Senha'/>
+            <button>Entrar</button>
+          </form>
+          <a href="">Criar Conta</a>
+        </div>
+      </div>
     </>
   )
 }
